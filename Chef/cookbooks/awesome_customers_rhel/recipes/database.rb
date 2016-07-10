7f0db0f5803d348b51bc42a08e0c2fbccbcc0e19
Sohan Akolkar <iSohan@iSohMac.local>
2016-07-10 01:27:24 -0700

    cleaning chef filees for awesome_customer_rhel recipe and comitting final files

---
+++ Chef/cookbooks/awesome_customers_rhel/recipes/database.rb
@@ -0,0 +1,62 @@
+#
+# Cookbook Name:: awesome_customers_rhel
+# Recipe:: database
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+# Configure the MySQL client
+
+mysql_client 'default' do
+  action :create
+end
+
+# Configure the MySQL service
+mysql_service 'default' do
+  initial_root_password node['awesome_customers_rhel']['database']['root_password']
+  action [:create, :start]
+end
+
+# Install the mysql2 Ruby gem.
+mysql2_chef_gem 'default' do
+  action :install
+end
+
+# Create the database instance
+mysql_database node['awesome_customers_rhel']['database']['dbname'] do
+  connection(
+    :host => node['awesome_customers_rhel']['database']['host'],
+    :username => node['awesome_customers_rhel']['database']['root_username'],
+    :password => node['awesome_customers_rhel']['database']['root_password']
+  )
+  action :create
+end
+
+# Add a database user
+mysql_database_user node['awesome_customers_rhel']['database']['admin_username'] do
+  connection(
+    :host => node['awesome_customers_rhel']['database']['host'],
+    :username => node['awesome_customers_rhel']['database']['root_username'],
+    :password => node['awesome_customers_rhel']['database']['root_password']
+  )
+  password node['awesome_customers_rhel']['database']['admin_password']
+  database_name node['awesome_customers_rhel']['database']['dbname']
+  host node['awesome_customers_rhel']['database']['host']
+  action [:create, :grant]
+end
+
+# Create a path to SQL file in the CHef cache
+create_tables_script_path = File.join(Chef::Config[:file_cache_path], 'create-tables.sql')
+
+# Write the SQL script to the filesystem
+cookbook_file create_tables_script_path do
+  source 'create-tables.sql'
+  owner 'root'
+  group 'root'
+  mode '0600'
+end
+
+# Seed the database with a table and test data.
+execute "initialize #{node['awesome_customers_rhel']['database']['dbname']} database" do
+  command "mysql -h #{node['awesome_customers_rhel']['database']['host']} -u #{node['awesome_customers_rhel']['database']['admin_username']} -p#{node['awesome_customers_rhel']['database']['admin_password']} -D #{node['awesome_customers_rhel']['database']['dbname']} < #{create_tables_script_path}"
+  not_if  "mysql -h #{node['awesome_customers_rhel']['database']['host']} -u #{node['awesome_customers_rhel']['database']['admin_username']} -p#{node['awesome_customers_rhel']['database']['admin_password']} -D #{node['awesome_customers_rhel']['database']['dbname']} -e 'describe customers;'"
+end
